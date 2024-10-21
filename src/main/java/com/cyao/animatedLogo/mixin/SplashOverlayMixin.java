3bad2d2d996404a5f52cb44f16eae69842195ba2
cheyao <94928179+cheyao@users.noreply.github.com>
2024-10-21 14:21:28 +0200

    Main logo animation

--- src/main/java/com/cyao/animatedLogo/mixin/SplashOverlayMixin.java
+++ src/main/java/com/cyao/animatedLogo/mixin/SplashOverlayMixin.java
@@ -1,26 +1,47 @@
 package com.cyao.animatedLogo.mixin;
 
-import com.cyao.animatedLogo.AnimationFrameTexture;
+import com.cyao.animatedLogo.AnimatedLogo;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.SplashOverlay;
+import net.minecraft.resource.ResourceReload;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SplashOverlay.class)
 public class SplashOverlayMixin {
-    @Shadow @Final private MinecraftClient client;
+    @Shadow @Final private ResourceReload reload;
+    @Shadow private float progress;
     @Unique
     private int count = 0;
     private Identifier frames[];
     private boolean inited = false;
+    private static int FRAMES = 12;
+    private static int IMAGE_PER_FRAME = 4;
+    private static int FRAMES_PER_FRAME = 3;
+
+    @ModifyArg(method = "render",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIFFIIII)V", ordinal = 0),
+            index = 3
+    )
+    private int removeText1(int i) {
+        return 0;
+    }
+    @ModifyArg(method = "render",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIFFIIII)V", ordinal = 1),
+            index = 3
+    )
+    private int removeText2(int i) {
+        return 0;
+    }
 
     /**
      * Draws the logo shadows
@@ -43,21 +64,26 @@ public class SplashOverlayMixin {
     )
     private void onAfterRenderLogo(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci, @Local(ordinal = 2) int scaledWidth, @Local(ordinal = 3) int scaledHeight, @Local long now, @Local(ordinal = 1) float fadeOutProgress, @Local(ordinal = 2) float fadeInProgress, @Local(ordinal = 3) float alpha, @Local(ordinal = 4) int x, @Local(ordinal = 5) int y, @Local(ordinal = 0) double height, @Local(ordinal = 6) int halfHeight, @Local(ordinal = 1) double width, @Local(ordinal = 7) int halfWidth) {
         if (!inited) {
-            this.frames = new Identifier[12];
+            this.frames = new Identifier[FRAMES];
 
-            for (int i = 0; i < 12; i++) {
+            for (int i = 0; i < FRAMES; i++) {
                 this.frames[i] = Identifier.of("animated-logo", "textures/gui/frame_" + i + ".png");
             }
 
             inited = true;
         }
 
-        context.drawTexture(this.frames[count], 0, y - halfHeight, halfWidth, (int) height, -0.0625F, 0, 120, 60, 120, 120);
-        context.drawTexture(this.frames[count], x, y - halfHeight, halfWidth, (int) height, 0.0625F, 60, 120, 60, 120, 120);
+        float progress = MathHelper.clamp(this.progress * 0.95F + this.reload.getProgress() * 0.050000012F, 0.0F, 1.0F);
 
-        count++;
-        if (count >= 12) {
-            count = 0;
+        context.drawTexture(this.frames[count / IMAGE_PER_FRAME / FRAMES_PER_FRAME], x - halfWidth, y - halfHeight, (int) width, (int)height,
+                0, 256 * ((count % (IMAGE_PER_FRAME * FRAMES_PER_FRAME)) / FRAMES_PER_FRAME), 1024, 256, 1024, 1024);
+
+        if (progress <= 0.8 || count != FRAMES * IMAGE_PER_FRAME * FRAMES_PER_FRAME - 1) {
+            count++;
+
+            if (count >= FRAMES * IMAGE_PER_FRAME * FRAMES_PER_FRAME) {
+                count = 0;
+            }
         }
     }
 }
