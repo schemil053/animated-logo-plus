f0c576d7dff4daaff63e01f0e1fb1722c76bce68
cheyao <94928179+cheyao@users.noreply.github.com>
2024-10-23 12:01:37 +0200

    Update for minecraft 1.21.1

--- src/main/java/com/cyao/animatedLogo/mixin/SplashOverlayMixin.java
+++ src/main/java/com/cyao/animatedLogo/mixin/SplashOverlayMixin.java
@@ -3,8 +3,10 @@ package com.cyao.animatedLogo.mixin;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.SplashOverlay;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.resource.ResourceReload;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ColorHelper;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -35,17 +37,17 @@ public class SplashOverlayMixin {
     private float f = 0;
 
     @ModifyArg(method = "render",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIFFIIII)V", ordinal = 0),
-            index = 3
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIFFIIIIIII)V", ordinal = 0),
+            index = 7
     )
     private int removeText1(int i) {
         return 0;
     }
     @ModifyArg(method = "render",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIFFIIII)V", ordinal = 1),
-            index = 3
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIFFIIIIIII)V", ordinal = 1),
+            index = 7
     )
-    private int removeText2(int i) {
+    private int removeText2(int u) {
         return 0;
     }
 
@@ -54,9 +56,6 @@ public class SplashOverlayMixin {
      * Original names for future reference:
      * @param scaledWidth i
      * @param scaledHeight j
-     * @param now l
-     * @param fadeOutProgress f
-     * @param fadeInProgress g
      * @param alpha s
      * @param x t
      * @param y u
@@ -66,9 +65,10 @@ public class SplashOverlayMixin {
      * @param halfWidth w
      */
     @Inject(method = "render",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIFFIIII)V", ordinal = 1, shift = At.Shift.AFTER)
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIFFIIIIIII)V", ordinal = 1, shift = At.Shift.AFTER)
     )
-    private void onAfterRenderLogo(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci, @Local(ordinal = 2) int scaledWidth, @Local(ordinal = 3) int scaledHeight, @Local long now, @Local(ordinal = 1) float fadeOutProgress, @Local(ordinal = 2) float fadeInProgress, @Local(ordinal = 3) float alpha, @Local(ordinal = 4) int x, @Local(ordinal = 5) int y, @Local(ordinal = 0) double height, @Local(ordinal = 6) int halfHeight, @Local(ordinal = 1) double width, @Local(ordinal = 7) int halfWidth) {
+    private void onAfterRenderLogo(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci,
+                                   @Local(ordinal = 2) int scaledWidth, @Local(ordinal = 3) int scaledHeight, @Local(ordinal = 3) float alpha, @Local(ordinal = 4) int x, @Local(ordinal = 5) int y, @Local(ordinal = 0) double height, @Local(ordinal = 6) int halfHeight, @Local(ordinal = 1) double width, @Local(ordinal = 7) int halfWidth) {
         if (!inited) {
             this.frames = new Identifier[FRAMES];
 
@@ -81,15 +81,14 @@ public class SplashOverlayMixin {
 
         float progress = MathHelper.clamp(this.progress * 0.95F + this.reload.getProgress() * 0.050000012F, 0.0F, 1.0F);
 
-        context.drawTexture(this.frames[count / IMAGE_PER_FRAME / FRAMES_PER_FRAME], x - halfWidth, y - halfHeight, (int) width, (int)height,
-                0, 256 * ((count % (IMAGE_PER_FRAME * FRAMES_PER_FRAME)) / FRAMES_PER_FRAME), 1024, 256, 1024, 1024);
+        context.drawTexture(RenderLayer::getGuiTextured, this.frames[count / IMAGE_PER_FRAME / FRAMES_PER_FRAME], x - halfWidth, y - halfHeight,
+                0, 256 * ((count % (IMAGE_PER_FRAME * FRAMES_PER_FRAME)) / FRAMES_PER_FRAME), (int) width, (int)height, 1024, 256, 1024, 1024, ColorHelper.getWhite(alpha));
 
         if (progress >= 0.8) {
             f = Math.min(alpha, f + 0.2f);
 
-            context.setShaderColor(1.0F, 1.0F, 1.0F, f);
-            context.drawTexture(Identifier.of("animated-logo", "textures/gui/studios.png"), x - halfWidth / 2, (int) (y - halfHeight + height), (int) (width / 2), (int) (height / 5),
-                    0, 0, 450, 50, 512, 512);
+            context.drawTexture(RenderLayer::getGuiTextured, Identifier.of("animated-logo", "textures/gui/studios.png"), x - halfWidth / 2, (int) (y - halfHeight + height),
+                    0, 0, halfWidth, (int) (height / 5), 450, 50, 512, 512, ColorHelper.getWhite(f));
         }
 
 	// Uncomment to loop
